674eb4fa620fcec974c3fd78fde3857ed9580ff5
Martin Viidik <martinviidik@gmail.com>
2020-03-17 11:12:17 +0200

    Scene transition experimentation, added TextMesh Pro

---
+++ Assets/Scripts/UI/MenuController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        Scenemanager.Instance.unloadScene(1);
+        Scenemanager.Instance.loadScene();
+    }
+}
---
+++ Assets/Scripts/Utility/Scenemanager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scenemanager : MonoBehaviour
+{
+    private static Scenemanager _instance;
+    public static Scenemanager Instance
+    {
+        get { return _instance; }
+    }
+
+    public Scene[] game_scenes;
+
+    void Start()
+    {
+        SceneManager.LoadScene("MainMenu", LoadSceneMode.Additive);
+        SceneManager.sceneUnloaded += OnSceneUnloaded;
+    }
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        _instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void loadScene()
+    {
+        SceneManager.LoadScene(2, LoadSceneMode.Additive);
+    }
+
+    public void unloadScene(int index)
+    {
+        SceneManager.UnloadSceneAsync(index);
+    }
+
+    private void OnSceneUnloaded(Scene current)
+    {
+        Debug.Log("OnSceneUnloaded: " + current);
+    }
+}
