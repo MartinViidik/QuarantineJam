a61516f1675b917d2106a9ac43e2a969c5ba4f83
Martin Viidik <martinviidik@gmail.com>
2020-03-20 16:29:11 +0200

    Framelimit, pause, other fixes

---
+++ Assets/Scripts/UI/Pause.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    public GameObject pausedGraphic;
+    public void PauseGame()
+    {
+        if (Time.timeScale == 1.0f)
+        {
+            Time.timeScale = 0.0f;
+            pausedGraphic.SetActive(true);
+        } else {
+            Time.timeScale = 1.0f;
+            pausedGraphic.SetActive(false);
+        }
+    }
+}
